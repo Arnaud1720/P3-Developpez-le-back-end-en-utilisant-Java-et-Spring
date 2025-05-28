b64be38f75d389c07b62f4a775553d19983e7f55
Arnaud <derisbourgarnaud@gmail.com>
2025-05-28 17:23:10 +0200

    ajout des controller User && Rentals

--- backend/src/main/java/com/arnaud/p3/ChaTop/controller/RentalsController.java
+++ backend/src/main/java/com/arnaud/p3/ChaTop/controller/RentalsController.java
@@ -1,2 +1,71 @@
-package com.arnaud.p3.ChaTop.controller;public class RentalsController {
+package com.arnaud.p3.ChaTop.controller;
+
+import com.arnaud.p3.ChaTop.dto.RentalsDto;
+import com.arnaud.p3.ChaTop.dto.UsersDto;
+import com.arnaud.p3.ChaTop.services.RentalServices;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/rentals")
+public class RentalsController {
+  private final RentalServices service;
+
+  public RentalsController(RentalServices service) {
+    this.service = service;
+  }
+
+  @PostMapping("/save")
+  public ResponseEntity<RentalsDto> createRental(
+    @RequestBody @Valid RentalsDto dto
+  ) {
+    // Le dto doit contenir dto.getOwnerId()
+    RentalsDto created = service.saveRental(dto);
+
+    URI location = ServletUriComponentsBuilder
+      .fromCurrentRequest()
+      .path("/{id}")
+      .buildAndExpand(created.getId())
+      .toUri();
+
+    return ResponseEntity
+      .created(location)
+      .body(created);
+  }
+
+  // Optionnel : récupérer toutes les locations
+  @GetMapping("/list")
+  public ResponseEntity<Collection<RentalsDto>> getAllRentals() {
+    return ResponseEntity.ok(service.getAllRentals());
+  }
+
+  @GetMapping("/find-rentals/{id}")
+  public ResponseEntity<RentalsDto> findRentalById(@PathVariable int id) {
+    return ResponseEntity.ok(service.findRentalById(id));
+  }
+
+  @DeleteMapping("/delete-rentals/{id}")
+  public ResponseEntity<Void> deleteRentalById(@PathVariable int id) {
+    service.deleteRentalById(id);
+    return ResponseEntity.noContent().build();
+  }
+  @PutMapping(path = "/rentals/{rentalId}",
+  consumes = MediaType.APPLICATION_JSON_VALUE,
+  produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<RentalsDto> updateRental(
+    @PathVariable int rentalId,
+    @RequestBody @Valid RentalsDto dto
+  ) {
+    dto.setId(rentalId);
+    RentalsDto updated = service.updateRental(dto);
+    return ResponseEntity.ok(updated);
+  }
+
+
 }
--- backend/src/main/java/com/arnaud/p3/ChaTop/controller/UsersController.java
+++ backend/src/main/java/com/arnaud/p3/ChaTop/controller/UsersController.java
@@ -2,27 +2,61 @@ package com.arnaud.p3.ChaTop.controller;
 
 import com.arnaud.p3.ChaTop.dto.UsersDto;
 import com.arnaud.p3.ChaTop.services.UsersServices;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
-    private final UsersServices userService;
 
-    public UsersController(UsersServices userService) {
-        this.userService = userService;
+  private final UsersServices service;
+
+  public UsersController(UsersServices service) {
+    this.service = service;
+  }
+
+
+  @PostMapping
+  public ResponseEntity<UsersDto> createUtilisateur(
+    @RequestBody @Valid UsersDto dto) {
+
+    UsersDto created = service.save(dto);
+
+    URI location = ServletUriComponentsBuilder
+      .fromCurrentRequest()
+      .path("/{id}")
+      .buildAndExpand(created.getId())
+      .toUri();
+
+    return ResponseEntity
+      .created(location)
+      .body(created);
+  }
+
+  @GetMapping("/{id}")
+    public ResponseEntity<UsersDto> getUserById(@PathVariable int id) {
+        UsersDto user = service.findById(id);
+        return ResponseEntity.ok(user);
+    }
+    @GetMapping("by-name/{name}")
+  public ResponseEntity<UsersDto> getUserByUsername(@PathVariable String name) {
+    UsersDto user = service.findByName(name);
+    return ResponseEntity.ok(user);
+    }
+    @GetMapping("by-email/{email}")
+  public ResponseEntity<UsersDto> getUserByEmail(@PathVariable String email) {
+    UsersDto user = service.findByEmail(email);
+    return ResponseEntity.ok(user);
     }
 
-    @PostMapping
-    public ResponseEntity<UsersDto> create(@RequestBody UsersDto usersDto) {
-        UsersDto created = userService.save(usersDto);
-        URI location = URI.create("/api/users/" + created.getId());
-        return ResponseEntity.created(location).body(created);
-    }
+  @DeleteMapping("delete/{id}")
+  public ResponseEntity<Void> delete(@PathVariable int id) {
+    service.deleteById(id);
+    return ResponseEntity.noContent().build();
+  }
+
 }
